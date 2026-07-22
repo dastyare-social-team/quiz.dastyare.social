aa298fd0503cb8d5fa4aad0589f3d76f930b0da4
Omid Shabab <hey@omidshabab.com>
2026-07-21 23:00:43 -0700

    feat: restructure routes and add versioned pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,7 @@
 import Hero from "@/components/sections/hero";
 
 const Page = () => {
-  return (
-    <div className="flex flex-col">
-      <Hero />
-    </div>
-  );
+  return <div className="flex flex-col">{/* <Hero /> */}</div>;
 };
 
 export default Page;
